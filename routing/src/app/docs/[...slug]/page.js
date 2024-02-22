31354a0f91a77ae3212cb49887a32a5efa56f90d
Vishal <vishalpal1501@gmail.com>
2024-02-22 15:22:42 +0530

    Scenario 6: Catch all segment

---
+++ routing/src/app/docs/[...slug]/page.js
@@ -0,0 +1,7 @@
+export default function DocsPage({ params }) {
+  return (
+    <>
+      <h1>Docs Page</h1>
+    </>
+  );
+}
---
+++ routing/src/app/docs/page.js
@@ -0,0 +1,7 @@
+export default function Docs() {
+  return (
+    <>
+      <h2>Docs</h2>
+    </>
+  );
+}
