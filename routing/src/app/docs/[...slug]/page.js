3f0fa2adb839301d358e9a00915c162bd5f3be7b
Vishal <vishalpal1501@gmail.com>
2024-02-22 15:27:32 +0530

    Scenario 6: Conditional catch all segment

--- routing/src/app/docs/[...slug]/page.js
+++ routing/src/app/docs/[...slug]/page.js
@@ -1,4 +1,20 @@
 export default function DocsPage({ params }) {
+  if (params.slug.length == 2) {
+    return (
+      <>
+        <h1>
+          Viewing the docs {params.slug[0]} and feature {params.slug[1]}
+        </h1>
+      </>
+    );
+  }
+  if (params.slug.length == 1) {
+    return (
+      <>
+        <h1>Viewing the docs {params.slug[0]}</h1>
+      </>
+    );
+  }
   return (
     <>
       <h1>Docs Page</h1>
