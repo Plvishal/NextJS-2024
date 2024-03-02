227f0bfe20ca8dad01fc87e32613ed789bf6b377
Vishal <vishalpal1501@gmail.com>
2024-03-02 13:47:17 +0530

    animated tooltip and wavy background

--- basicproject/src/app/page.tsx
+++ basicproject/src/app/page.tsx
@@ -1,5 +1,6 @@
 import FeaturedSection from '@/components/FeaturedSection';
 import HeroSection from '@/components/HeroSection';
+import Instructors from '@/components/Instructors';
 import TestimonialCard from '@/components/TestimonialCard';
 import UpcomingWebinar from '@/components/UpcomingWebinar';
 import WhyChooseUs from '@/components/WhyChooseUs';
@@ -13,6 +14,7 @@ export default function Home() {
         <WhyChooseUs />
         <TestimonialCard />
         <UpcomingWebinar />
+        <Instructors />
       </main>
     </>
   );
