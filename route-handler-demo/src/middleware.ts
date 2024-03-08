16fe9d82ea2550187569f6875868ecedb2bd34d3
Vishal <vishalpal1501@gmail.com>
2024-03-08 10:50:05 +0530

    Middleware Approach 1:  Matcher and redirect

---
+++ route-handler-demo/src/middleware.ts
@@ -0,0 +1,8 @@
+import { NextResponse, type NextRequest } from 'next/server';
+export function middleware(request: NextRequest) {
+  return NextResponse.redirect(new URL('/', request.url));
+}
+
+export const config = {
+  matcher: '/profile',
+};
